41f94b9deaa024b6e36af0217f444de36dc89218
Caleb Steckler <stecklec@email.sc.edu>
2026-03-17 09:17:39 -0400

    React App V1

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Layout from './Layout';
+import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path = "/" element = {<Layout />}>
+          <Route index element = {<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../css/Home.css";
+
+const Home = () => {
+    return (
+        <main id="home" className="main-content">
+            <h1 id='title'>Hello World</h1>
+            <p>Home Page p tag here</p>
+        </main>
+    );
+};
+
+export default Home;
